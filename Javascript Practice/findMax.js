e94ac8ebd9ce4ad7c0174417b602cecf4a175f2b
Meghan Koda <Meghan.koda@libertymutual.com>
2018-01-19 16:08:38 -0500

    Restaurantcod3

---
+++ Javascript Practice/findMax.js
@@ -0,0 +1,17 @@
+var arrayMax[]
+alert("Please set arrayMax to a valid array")
+
+function findMax(arrayMax[]){
+	if(arrayMax!=null){
+		for(var arrayPosition=0
+		
+		
+    console.log("Array position searching in: " + str);
+    console.log("Character to find: " + search);
+    console.log("Number of times the character appears: " + count);
+
+	}else{
+		alert("Please set arrayMax to a valid array.")
+	}
+}
+	
